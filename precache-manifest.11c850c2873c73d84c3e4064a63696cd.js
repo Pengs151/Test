2e991e493dde519e725b13f81a745255dbd574fa
ricardo <r3molina@Ucsd.edu>
2018-11-01 22:07:12 -0700

    Updates

---
+++ precache-manifest.11c850c2873c73d84c3e4064a63696cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9201d40606519ca2549c3482c9aaf7e",
+    "url": "/Test/static/media/Spyro.c9201d40.jpg"
+  },
+  {
+    "revision": "905a85fb29903b0c9e8fdc90e9f7299a",
+    "url": "/Test/static/media/03_NEW-TO-DnD_Races_InlineR_RacesGeneral.905a85fb.jpg"
+  },
+  {
+    "revision": "658d72dc480d2e8be55b",
+    "url": "/Test/static/js/runtime~main.658d72dc.js"
+  },
+  {
+    "revision": "3cd3e12dd21e11c71be4",
+    "url": "/Test/static/js/main.3cd3e12d.chunk.js"
+  },
+  {
+    "revision": "0b730b094abeaa7a4c88",
+    "url": "/Test/static/js/1.0b730b09.chunk.js"
+  },
+  {
+    "revision": "3cd3e12dd21e11c71be4",
+    "url": "/Test/static/css/main.3964da90.chunk.css"
+  },
+  {
+    "revision": "4ac160c8d70f666861c20973b67ad89f",
+    "url": "/Test/index.html"
+  }
+];
