40a46a9648ec3842036fae1104fdbd886b067ce7
ricardo <r3molina@Ucsd.edu>
2018-11-06 23:23:51 -0800

    hellos

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -3,41 +3,55 @@ import React, { Component } from 'react';
 export default class StatBox extends Component {
   constructor(props){
     super(props);
-    this.state = "";
+    this.state = {input:""};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmiut = this.handleSubmiut.bind(this);
   }
 
-  handleChange(event, statName){
-    this.setState({input: event.target.value})
-    
-    this.updateInput(statName, this.setState);
+  handleChange(event){
+    this.setState({value: event.target.value});
+    this.updateInput(this.props.Stat, event);
   }
+
+  handleSubmiut(event){
+    event.preventDefault();
+  }
+
   updateInput(key, event){
     console.log(key);
-    this.setState(key: event.target.value);
+   // this.setState({input: event.target.value});
     localStorage.setItem(key,event.target.value);
   }
+    
   
   checkValue(key){
     if(localStorage.hasOwnProperty(key)){
-      var value = localStorage.getItem(key);
-      this.setState = value;
+      this.setState({ value:localStorage.getItem(key)});
+      console.log(localStorage.getItem(key));
+      console.log("hellooo");
     }
-    else{
-      value = this.state.key
     }
-
-   
-    }
-  
+    
+  componentDidMount(){
+    this.checkValue(this.props.Stat);
+  }
+/*
+  componentWillUnmount(){
+      this.updateInput(this.props.Stat, this.state.input);
+      console.log("hellloooooo");
+  }
+  */
     render() {
+      //  this.checkValue();
         return (
-            <div className="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+            
+            <div className="btn-toolbar mb-6" role="toolbar" aria-label="Toolbar with button groups">
             <div className="input-group">
               <div className="input-group-prepend">
                 <div className="input-group-text" id="btnGroupAddon">{this.props.Stat}</div>
               </div>
-              <div className="col-2">
-              <input type="text" name = {this.props.Stat}  value ={this.checkValue.bind(this.props.Stat)} onChange={this.updateInput.bind(this.props.Stat, this)} className="form-control"/>
+              <div className="col-6">
+              <input type="text" name = {this.props.Stat}  value ={this.state.value} onChange={this.handleChange}  className="form-control"/>
               </div>
               <button type="button" className="btn btn-secondary">+</button>
             </div>
